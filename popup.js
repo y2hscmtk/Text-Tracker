105d863862759c0a9a30a5631c2e9fe781461d04
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-02 23:29:08 +0900

    FIX : CSS & UI 변경

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var textLengthTotal = text.length;
         var textLengthNoSpaces = text.replace(/\s/g, '').length; // 공백 제거 후 글자 수 계산
 
-        countDisplayTotal.textContent = textLengthTotal + ' 글자 (공백 포함)';
-        countDisplayNoSpaces.textContent = textLengthNoSpaces + ' 글자 (공백 제외)';
+        countDisplayTotal.textContent = ' 공백 포함 : ' + textLengthTotal
+        countDisplayNoSpaces.textContent = ' 공백 제외 : ' + textLengthNoSpaces
     });
 });
