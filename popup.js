cccabdd2add1ea8697f44805a96bde52f284d7af
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-02 22:58:00 +0900

    FEAT : 공백포함, 공백제외 문자 카운팅

--- popup.js
+++ popup.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     var textInput = document.getElementById('textInput');
-    var countDisplay = document.getElementById('count');
+    var countDisplayTotal = document.getElementById('countTotal');
+    var countDisplayNoSpaces = document.getElementById('countNoSpaces');
+
     textInput.addEventListener('input', function () {
-        var textLength = textInput.value.length;
-        countDisplay.textContent = textLength + ' 글자';
+        var text = textInput.value;
+        var textLengthTotal = text.length;
+        var textLengthNoSpaces = text.replace(/\s/g, '').length; // 공백 제거 후 글자 수 계산
+
+        countDisplayTotal.textContent = textLengthTotal + ' 글자 (공백 포함)';
+        countDisplayNoSpaces.textContent = textLengthNoSpaces + ' 글자 (공백 제외)';
     });
 });
